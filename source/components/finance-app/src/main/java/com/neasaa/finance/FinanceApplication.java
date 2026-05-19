c0ebc087adc2dc23325f7e90d4150ff6305192d2
Vijay <vijay_garry@hotmail.com>
2026-05-18 20:12:43 -0400

    Initial setup of spring boot application.

---
+++ source/components/finance-app/src/main/java/com/neasaa/finance/FinanceApplication.java
@@ -0,0 +1,12 @@
+package com.neasaa.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class FinanceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceApplication.class, args);
+    }
+}
